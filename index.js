d7678406a663061eda10903a9d5798ef22bf96da
AnaGras <anagrasmas98@gmail.com>
2025-12-19 09:44:31 +0100

    rutas GET funcionales

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), function() {
+    console.log("Hola desde el puerto: " + app.get("port"));
+});
+
+app.use(require("./src/routes/indexrutas"));
+
